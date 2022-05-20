4d706161a9534296a0afc88a984716e15d03ec22
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2022-05-20 12:16:58 +0200

    reco

--- pages/hello.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/Hello.module.css'
-
-const Hello =() =>{
-    console.log('Styles for hello',styles)
-    return <div className={styles.container}>Hello World!</div>;
-};
-
-export default Hello;
